a012f0489b122635bd0291af598baa83bfe2658f
DulceGr <fmarigd@gmail.com>
2023-03-21 22:44:41 -0600

    Expression tree classes

---
+++ ProyectoLFA/ProyectoLFA/Classes/ET.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Classes
+{
+    public class ET
+    {
+        public Node root;
+        public string expression;
+
+        public ET()
+        {
+            root = null;
+        }
+        private void ShutingYard()
+        {
+
+        }
+    }
+
+}
---
+++ ProyectoLFA/ProyectoLFA/Classes/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Classes
+{
+    public class Node
+    {
+        public string expresion;
+        public Node Left;
+        public Node Right;
+    }
+}
