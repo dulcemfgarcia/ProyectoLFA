e48f4bd1120458f0b5333ff27b16d80ea99a68aa
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-03-28 11:58:24 -0600

    Create Token.cs

---
+++ ProyectoLFA/ProyectoLFA/Classes/Token.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_LFA.Utilities.Clases
+{
+    public class Token
+    {
+        /// <summary>
+        /// Number defined in file
+        /// </summary>
+        public int TokenNumber;
+
+        /// <summary>
+        /// List of ascii codes for the last character in an individual token.
+        /// </summary>
+        public List<char> FirstPositions = new List<char>();
+
+        /// <summary>
+        /// List of ascii codes for the last character in an individual token.
+        /// </summary>
+        public List<char> LastPositions = new List<char>();
+
+        public Token(int tokenNumber, List<char> FirstPosition, List<char> LastPosition)
+        {
+            this.TokenNumber = tokenNumber;
+            this.FirstPositions = FirstPosition;
+            this.LastPositions = LastPosition;
+        }
+    }
+}
