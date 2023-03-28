946f947d1570e626cfe913965a38bfc03c5d31a2
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-03-28 11:56:56 -0600

    Create Action.cs

---
+++ ProyectoLFA/ProyectoLFA/Classes/Action.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Proyecto_LFA.Utilities.Clases
+{
+    public class Action
+    {
+        public string ActionName;
+        public Dictionary<int, string> ActionValues = new Dictionary<int, string>();
+    }
+}
