aa2528ee932252bd956adbd7734fc08b172b3c39
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-03-28 14:17:06 -0600

    Update FileAnalyser.Designer.cs

--- ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
+++ ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
@@ -36,6 +36,7 @@ namespace ProyectoLFA
             this.TXTPath = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BTNUpload
@@ -113,12 +114,25 @@ namespace ProyectoLFA
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(243, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 33);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Detalles";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FileAnalyser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(549, 458);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TXTPath);
@@ -127,7 +141,7 @@ namespace ProyectoLFA
             this.Controls.Add(this.RTBGrammar);
             this.Controls.Add(this.BTNUpload);
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form1";
+            this.Name = "FileAnalyser";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -144,6 +158,7 @@ namespace ProyectoLFA
         private System.Windows.Forms.TextBox TXTPath;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
