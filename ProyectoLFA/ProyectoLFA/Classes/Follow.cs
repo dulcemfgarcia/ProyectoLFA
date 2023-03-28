8ba6011df0394321c25a0063b2a1aa269c19c272
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-03-28 11:58:31 -0600

    Create Follow.cs

---
+++ ProyectoLFA/ProyectoLFA/Classes/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_LFA.Utilities
+{
+    public class Follow
+    {
+        public string character { get; set; }
+        public bool isAcceptanceStatus { get; set; }
+        public List<int> follows { get; set; }
+
+        public Follow(string character)
+        {
+            this.character = character;
+            isAcceptanceStatus = character == CharSET.EndCharacter;
+            follows = new List<int>();
+        }
+    }
+}
