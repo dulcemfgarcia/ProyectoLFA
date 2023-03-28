9d7a3f93d5f1f03ba40b208f91a615388ef707a1
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-03-28 14:17:11 -0600

    Update Program.cs

--- ProyectoLFA/ProyectoLFA/Program.cs
+++ ProyectoLFA/ProyectoLFA/Program.cs
@@ -21,7 +21,7 @@ namespace ProyectoLFA
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form3());
         }
     }
 }
