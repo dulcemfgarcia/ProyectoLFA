07b01c950cdba9f9814be89cdc559b5f5df8620f
Angie-Schumann <98908820+Angie-Schumann@users.noreply.github.com>
2023-05-06 13:08:31 -0600

    Update FileAnalyser.Designer.cs

--- ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
+++ ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
@@ -37,6 +37,7 @@ namespace ProyectoLFA
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.TransitionBTN = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BTNUpload
@@ -44,9 +45,10 @@ namespace ProyectoLFA
             this.BTNUpload.BackColor = System.Drawing.SystemColors.ControlText;
             this.BTNUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNUpload.ForeColor = System.Drawing.SystemColors.Control;
-            this.BTNUpload.Location = new System.Drawing.Point(10, 22);
+            this.BTNUpload.Location = new System.Drawing.Point(13, 27);
+            this.BTNUpload.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BTNUpload.Name = "BTNUpload";
-            this.BTNUpload.Size = new System.Drawing.Size(118, 23);
+            this.BTNUpload.Size = new System.Drawing.Size(157, 28);
             this.BTNUpload.TabIndex = 0;
             this.BTNUpload.Text = "Cargar Gramática";
             this.BTNUpload.UseVisualStyleBackColor = false;
@@ -55,19 +57,20 @@ namespace ProyectoLFA
             // RTBGrammar
             // 
             this.RTBGrammar.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.RTBGrammar.Location = new System.Drawing.Point(12, 84);
+            this.RTBGrammar.Location = new System.Drawing.Point(16, 103);
+            this.RTBGrammar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RTBGrammar.Name = "RTBGrammar";
-            this.RTBGrammar.Size = new System.Drawing.Size(370, 241);
+            this.RTBGrammar.Size = new System.Drawing.Size(492, 296);
             this.RTBGrammar.TabIndex = 1;
             this.RTBGrammar.Text = "";
             this.RTBGrammar.TextChanged += new System.EventHandler(this.RTBGrammar_TextChanged);
             // 
             // TResult
             // 
-            this.TResult.Location = new System.Drawing.Point(136, 58);
-            this.TResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TResult.Location = new System.Drawing.Point(181, 71);
+            this.TResult.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TResult.Name = "TResult";
-            this.TResult.Size = new System.Drawing.Size(246, 20);
+            this.TResult.Size = new System.Drawing.Size(327, 22);
             this.TResult.TabIndex = 2;
             this.TResult.TextChanged += new System.EventHandler(this.TResult_TextChanged);
             // 
@@ -75,30 +78,29 @@ namespace ProyectoLFA
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(65, 61);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(87, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.Size = new System.Drawing.Size(84, 18);
             this.label1.TabIndex = 3;
             this.label1.Text = "Resultado:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // TXTPath
             // 
-            this.TXTPath.Location = new System.Drawing.Point(136, 27);
-            this.TXTPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TXTPath.Location = new System.Drawing.Point(181, 33);
+            this.TXTPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TXTPath.Name = "TXTPath";
-            this.TXTPath.Size = new System.Drawing.Size(246, 20);
+            this.TXTPath.Size = new System.Drawing.Size(327, 22);
             this.TXTPath.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Desktop;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(260, 336);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(347, 414);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 27);
+            this.button1.Size = new System.Drawing.Size(97, 33);
             this.button1.TabIndex = 5;
             this.button1.Text = "Regresar";
             this.button1.UseVisualStyleBackColor = false;
@@ -108,10 +110,10 @@ namespace ProyectoLFA
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Desktop;
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(337, 336);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(449, 414);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(62, 27);
+            this.button2.Size = new System.Drawing.Size(83, 33);
             this.button2.TabIndex = 6;
             this.button2.Text = "Salir";
             this.button2.UseVisualStyleBackColor = false;
@@ -121,21 +123,35 @@ namespace ProyectoLFA
             // 
             this.TransitionBTN.BackColor = System.Drawing.SystemColors.Desktop;
             this.TransitionBTN.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.TransitionBTN.Location = new System.Drawing.Point(183, 330);
-            this.TransitionBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TransitionBTN.Location = new System.Drawing.Point(160, 415);
+            this.TransitionBTN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TransitionBTN.Name = "TransitionBTN";
-            this.TransitionBTN.Size = new System.Drawing.Size(73, 38);
+            this.TransitionBTN.Size = new System.Drawing.Size(181, 32);
             this.TransitionBTN.TabIndex = 7;
             this.TransitionBTN.Text = "Obtener transiciones\r\n";
             this.TransitionBTN.UseVisualStyleBackColor = false;
             this.TransitionBTN.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(12, 415);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Generador Scanner";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
             // FileAnalyser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(412, 372);
+            this.ClientSize = new System.Drawing.Size(549, 458);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.TransitionBTN);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -144,6 +160,7 @@ namespace ProyectoLFA
             this.Controls.Add(this.TResult);
             this.Controls.Add(this.RTBGrammar);
             this.Controls.Add(this.BTNUpload);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FileAnalyser";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -162,6 +179,7 @@ namespace ProyectoLFA
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button TransitionBTN;
+        private System.Windows.Forms.Button button3;
     }
 }
 
